3cee3cb2d85a0c1a57e30221e12905f40408e450
vyomPriyesh <vyompriyesh@gmail.com>
2026-09-06 11:44:34 +0530

    Refactor Platforms component data handling
    Removed unused variable and adjusted data destructuring.

--- src/pages/Platforms.jsx
+++ src/pages/Platforms.jsx
@@ -29,11 +29,8 @@ const Platforms = () => {
         form.resetFields()
     }
 
-    const allPlatforms = {}
-
     const { 
-  //  data: { data: allPlatforms = {} } = {}, 
-        data,
+    data : allPlatforms = {},
     refetch: allPlatformsRefetch, 
     isFetching: isAllPlatformsFetching 
 } = useQuery({
@@ -141,9 +138,6 @@ const Platforms = () => {
     }
     return (
         <div className='flex flex-col gap-5'>
-            <pre style={{ backgroundColor: '#f4f4f4', padding: '10px', borderRadius: '5px' }}>
-  {JSON.stringify(data, null, 2)}
-</pre>
             <PageTitleAddbtn title='Platforms' add={canAdd} addClick={onCloseModal} />
             <TableUi
                 columns={columns}
